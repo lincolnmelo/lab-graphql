61261f780eada67e4a7b975a5c43ae45dd2b544e
Lincoln Melo <lincoln.melo@somoseducacao.com.br>
2019-08-30 11:57:44 -0300

    lab lincoln server ok

--- index.js
+++ index.js
@@ -26,44 +26,43 @@ type Query {
 
 const alunos = [{
   id: 1,
-  nome: "Lincoln",
-  sobrenome: "Morais de Melo",
-  idade: 30
+  nome: "João",
+  sobrenome: "silva",
+  idade: 30,
+  responsaveis: [{
+    parentesco: "Pai",
+    nome: "alexandre",
+    idade: 48,
+    aluno_id: 1
+  }]
 }, {
   id: 2,
-  nome: "Beatriz",
-  sobrenome: "Vicente de lemos",
-  idade: 26
+  nome: "Luna",
+  sobrenome: "puert",
+  idade: 26,
+  responsaveis: [{
+    parentesco: "Mãe",
+    nome: "Maria do Carmo",
+    idade: 53,
+    aluno_id: 2
+  }]
 }]
 
-const responsaveis = [{
+,
+responsaveis = [{
   parentesco: "Pai",
   nome: "alexandre",
   idade: 48,
   aluno_id: 1
 },{
-  parentesco: "Mãe",
-  nome: "Maria do Carmo",
-  idade: 53,
-  aluno_id: 2
+  
 }]
+
+
 
 var root = { 
     alunos: () => {
-      teste = alunos
-      teste.responsaveis = responsaveis
-
-      // teste.responsaveis = responsaveis.map(resp => {
-      //   if (responsaveis.aluno_id === alunos.aluno_id){
-      //     return resp
-      //   }
-      //   return []
-      // })
-      console.log("\n teste Alunos: ", teste)
-      return teste
-    },
-    responsaveis: () => {
-      return responsaveis
+     return alunos
     },
     nome: () => 'Teste Lincoln',
     data: () => new Date,
